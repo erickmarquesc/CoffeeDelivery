6408913a41dec26fdb9af59332b52b13602c0f87
erickmarquesc <erick.marquesc@hotmail.com>
2023-01-11 17:25:07 -0300

    feat: :sparkles: create a new component HeroSection

---
+++ src/pages/Home/components/HeroSection/index.tsx
@@ -0,0 +1,56 @@
+import { HeroSectionConteiner, ServicesHero, TitleHeroContent } from "./styles";
+import { Package, Coffee, ShoppingCart, Timer } from "phosphor-react";
+import marketingImg from "../../../../assets/Imagem.svg";
+
+export function HeroSection() {
+  return (
+    <HeroSectionConteiner>
+      <TitleHeroContent>
+        <h1 className="title">
+          Encontre o café perfeito para qualquer hora do dia
+        </h1>
+        <p className="subtitle">
+          Com o Coffee Delivery você recebe seu café onde estiver, a
+          qualquer hora
+        </p>
+
+        <ServicesHero>
+          <section>
+            <div className="left-column">
+              <div className="cart icon">
+                <ShoppingCart weight="fill" size={16} />
+              </div>
+              <p>Compra simples e segura</p>
+            </div>
+
+            <div className="left-column">
+              <div className="timer icon">
+                <Timer weight="fill" size={16} />
+              </div>
+              <p>Entrega rápida e rastreada</p>
+            </div>
+          </section>
+
+          <section>
+            <div className="right-column">
+              <div className="package icon">
+                <Package weight="fill" size={16} />
+              </div>
+              <p>Embalagem mantém o café intacto</p>
+            </div>
+            
+            <div className="right-column">
+              <div className="coffee icon">
+                <Coffee weight="fill" size={16} />
+              </div>
+              <p>O café chega fresquinho até você</p>
+            </div>
+          </section>
+
+        </ServicesHero>
+      </TitleHeroContent>
+
+      <img className="img" src={marketingImg} alt="" />
+    </HeroSectionConteiner>
+  );
+};
---
+++ src/pages/Home/components/HeroSection/styles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+
+export const HeroSectionConteiner = styled.div`
+  gap: 3.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 34px;
+
+  .img {
+    height: 22.5rem;
+    width: 29.75rem;
+    align-self: flex-end;
+  };
+`
+
+export const TitleHeroContent = styled.div`
+  width: 36.75rem;
+
+  .title {
+    margin-bottom: 16px;
+    font: normal 3rem 'Baloo 2', sans-serif;
+    color: ${(props) => props.theme['base-title']};
+  };
+
+  .subtitle {
+    font-size: 1.25rem;
+    margin-bottom: 4.125rem;
+    color: ${(props) => props.theme['base-text']};
+  };
+`
+
+export const ServicesHero = styled.div`
+  gap: 2.5rem;
+  display: flex;
+  font-size: 1rem;
+  align-items: center;
+  color: ${(props) => props.theme['base-text']};
+
+  svg{
+    color: ${(props) => props.theme['white']};
+  };
+
+  .right-column {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1.25rem;
+  };
+
+  .left-column {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1.25rem;
+  };
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 0.5rem;
+    width: 2rem;
+    border-radius: 9999px;
+
+    margin-right: 0.75rem;
+  };
+
+  .cart {
+    background: ${(props) => props.theme['yellow-dark']};
+  };
+
+  .timer {
+    background: ${(props) => props.theme['yellow']};
+  };
+
+  .package {
+    background: ${(props) => props.theme['base-text']};
+  };
+
+  .coffee {
+    background: ${(props) => props.theme['purple-dark']};
+  };
+`
