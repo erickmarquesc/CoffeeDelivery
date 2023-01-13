198f2dbddc2c81ec8d21170c0b1419c6fe9d4c69
erickmarquesc <erick.marquesc@hotmail.com>
2023-01-12 21:19:09 -0300

    feat: :zap: use hook useCart

--- src/components/Header/CartButton/index.tsx
+++ src/components/Header/CartButton/index.tsx
@@ -1,23 +1,35 @@
-import { ShoppingContext } from '../../../contexts/ShoppingCartContext';
+import { useCart } from '../../../contexts/ShoppingCartContext';
 import { ShoppingCart } from "phosphor-react";
 import { NavLink } from "react-router-dom";
 import { CartButton } from "./styles";
-import { useContext } from 'react';
 
 export function ShoppingCartButton() {
-  const { shoppingCartItems } = useContext(ShoppingContext);
+  
+  const {shoppingCartItems} = useCart();
   const cartCounter = shoppingCartItems.length;
 
   return (
-    <NavLink to="/checkout">
-      <CartButton>
-        <ShoppingCart size={20} className="cartIcon" weight="fill" />
-        {cartCounter > 0 ?
-          <div className="countOfItems">{cartCounter}</div>
-          :
-          <></>
-        }
-      </CartButton>
-    </NavLink>
+    <>
+      {cartCounter > 0 ?
+        <NavLink to="/checkout" >
+          <CartButton >
+            <ShoppingCart size={20} className="cartIcon" weight="fill" />
+            {cartCounter > 0 ?
+              <div className="countOfItems">{cartCounter}</div>
+              :
+              <></>
+            }
+          </CartButton>
+        </NavLink>
+        : <CartButton disabled>
+          <ShoppingCart size={20} className="cartIcon" weight="fill" />
+          {cartCounter > 0 ?
+            <div className="countOfItems">{cartCounter}</div>
+            :
+            <></>
+          }
+        </CartButton>
+      }
+    </>
   );
 };
