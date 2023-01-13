c904fad021a8226b8d2dd9edf65e4de7f8089dc2
erickmarquesc <erick.marquesc@hotmail.com>
2023-01-12 21:19:41 -0300

    feat: :sparkles: add links

--- src/components/Footer/MidiaButton/index.tsx
+++ src/components/Footer/MidiaButton/index.tsx
@@ -1,5 +1,4 @@
 import { FigmaLogo, GithubLogo } from "phosphor-react";
-import { NavLink } from "react-router-dom";
 import { SocialMidiaButton } from "./styles";
 
 interface MidiaButtonProps {
@@ -7,17 +6,19 @@ interface MidiaButtonProps {
 };
 
 export function MidiaButton({ midia }: MidiaButtonProps) {
+  const gitHubURL = "https://github.com/erickmarquesc/CoffeeDelivery";
+  const figmaURL = "https://www.figma.com/file/wGsjJE5mSRMmC2SF24Fyno/Coffee-Delivery?node-id=0%3A1&t=NmJP0wmgYhkEmcMt-1";
   return (
     <>
       {midia === "github" ?
-        <a href="https://github.com/erickmarquesc/CoffeeDelivery">
+        <a href={gitHubURL}>
 
           <SocialMidiaButton >
             <GithubLogo size={20} weight="fill" />
           </SocialMidiaButton>
         </a>
         :
-        <a href="https://www.figma.com/file/wGsjJE5mSRMmC2SF24Fyno/Coffee-Delivery?node-id=0%3A1&t=NmJP0wmgYhkEmcMt-1">
+        <a href={figmaURL}>
           <SocialMidiaButton>
             <FigmaLogo size={20} weight="bold" />
           </SocialMidiaButton>
