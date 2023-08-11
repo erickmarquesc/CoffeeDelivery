c4b199ff04b4784803c942d08aa2b9aa92147bbd
Erick Marques <erick.marquesc@hotmail.com>
2023-08-10 22:31:20 -0300

    perf: :art: format of the code

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,38 +1,34 @@
-import { ShoppingContext } from '../../contexts/ShoppingCartContext';
+import { useCart } from '../../contexts/ShoppingCartContext';
 import { ShoppingCart } from "phosphor-react";
 import { NavLink } from "react-router-dom";
 import { CartButton } from "./styles";
-import { useContext } from 'react';
 
 export function ShoppingCartButton() {
-  const { shoppingCartItems } = useContext(ShoppingContext);
+  const { shoppingCartItems } = useCart();
+  
   const cartCounter = shoppingCartItems.length;
-
+  const isActiveCart = cartCounter > 0 ? true : false;
+  
   return (
-    <>
-      {cartCounter > 0 ?
-        <NavLink to="/checkout">
-          <CartButton>
-            <ShoppingCart
-              size={20}
-              className="cartIcon"
-              weight="fill"
-            />
-            <div className="countOfItems">
-              {cartCounter}
-            </div>
-          </CartButton>
-        </NavLink>
-        :
-        <CartButton disabled>
-          <ShoppingCart size={20} className="cartIcon" weight="fill" />
-          {cartCounter > 0 ?
-            <div className="countOfItems">{cartCounter}</div>
-            :
-            <></>
-          }
-        </CartButton>
+    <NavLink
+      to={
+        isActiveCart
+          ? "/checkout"
+          : ''
       }
-    </>
+    >
+      <CartButton disabled={!isActiveCart}>
+        <ShoppingCart
+          size={20}
+          className="cartIcon"
+          weight="fill"
+        />
+        {isActiveCart && (
+          <div className="countOfItems">
+            {cartCounter}
+          </div>
+        )}
+      </CartButton>
+    </NavLink>
   );
 };
