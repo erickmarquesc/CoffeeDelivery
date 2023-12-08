b03b3d7baf63c5fcafb74a7587a6457694e01968
Erick Marques <erick.marquesc@hotmail.com>
2023-12-08 15:16:17 -0300

    perf: :construction: work in progress

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 
 import { DefaultLayout } from './layouts/DefaultLayout'
@@ -7,14 +7,12 @@ import { Confirmation } from './pages/Confirmation'
 
 export function Router() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Route>
+    </Routes>
   )
 }
